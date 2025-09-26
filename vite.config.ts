127f2d397ad771280dd16f0a9019e956c960e611
Neelesh_Kurmi <neeleshp212@gmail.com>
2025-09-27 01:39:50 +0530

    Initial commit with project files

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "https://Neeleshkurmi.github.io/E-Prabha",
   server: {
     host: "::",
     port: 8080,
